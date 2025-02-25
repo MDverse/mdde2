1ca03cc055a91be71f7327949280c24fa5b2e59a
GiuliaDGM <giuliadgm05@gmail.com>
2025-02-25 18:54:27 +0100

    Adding queries files to fill tables

---
+++ queries.py
@@ -0,0 +1,25 @@
+from sqlalchemy import func
+from sqlmodel import Session, select
+
+from db_engine import engine
+from db_models import Dataset, DatasetOrigin
+
+def get_dataset_origin_summary():
+    """
+    Returns a list of rows containing:
+        dataset_origin, number_of_datasets, first_dataset, last_dataset
+    """
+    with Session(engine) as session:
+        statement = (
+            select(
+                DatasetOrigin.name.label("dataset_origin"),
+                func.count(Dataset.dataset_id).label("number_of_datasets"),
+                func.min(Dataset.date_created).label("first_dataset"),
+                func.max(Dataset.date_created).label("last_dataset"),
+            )
+            .join(Dataset, Dataset.origin_id == DatasetOrigin.origin_id)
+            .group_by(DatasetOrigin.name)
+        )
+
+        results = session.exec(statement).all()
+        return results
