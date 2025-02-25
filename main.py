4a5bd971b7fcbcb35eaac7d8d6f63610a1f2b680
GiuliaDGM <giuliadgm05@gmail.com>
2025-02-25 18:55:50 +0100

    Adding homepage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
+from queries import get_dataset_origin_summary
+
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
