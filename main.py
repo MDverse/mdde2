47d7d0d7bb2a8f17f0e516b08ee4beceeebe3d66
GiuliaDGM <giuliadgm05@gmail.com>
2025-02-26 16:39:07 +0100

    Adding database query

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from queries import get_dataset_origin_summary
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def read_index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    # Get the data from your query
+    results, total_count = get_dataset_origin_summary()
+
+    # Pass it to the template
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "results": results,
+            "total_count": total_count,
+        }
+    )
