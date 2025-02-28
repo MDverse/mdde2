b43ff745428ae2037d38877a02002207433592ec
GiuliaDGM <giuliadgm05@gmail.com>
2025-02-28 16:15:45 +0100

    Starting search page

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def read_index(request: Request):
-    # Get the data your query
+    # Get the data from query
     datasets_stats_results, datasets_stats_total_count = get_dataset_origin_summary()
     file_type_stats_summary = get_file_type_stats()
 
@@ -26,3 +26,12 @@ async def read_index(request: Request):
         }
     )
 
+
+@app.get("/search")
+async def read_search(request: Request):
+    return templates.TemplateResponse(
+        "search.html",
+        {
+            "request": request,
+        }
+    )
