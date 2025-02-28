34df32057f90f7ee4d50492931e6c93828f3b6da
GiuliaDGM <giuliadgm05@gmail.com>
2025-02-28 13:59:17 +0100

    Adding file_type stats

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
-from queries import get_dataset_origin_summary, file_type_stats
+from queries import get_dataset_origin_summary, get_file_type_stats
 
 app = FastAPI()
 
@@ -13,7 +13,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 async def read_index(request: Request):
     # Get the data your query
     datasets_stats_results, datasets_stats_total_count = get_dataset_origin_summary()
-    file_type_stats_summary = file_type_stats()
+    file_type_stats_summary = get_file_type_stats()
 
     # Pass it to the template
     return templates.TemplateResponse(
--- queries.py
+++ queries.py
@@ -1,34 +1,8 @@
-from sqlalchemy import func, case
+from sqlalchemy import func
 from sqlmodel import Session, select
 
 from db_engine import engine
 from db_models import Dataset, DatasetOrigin, File, FileType
-
-# def get_dataset_origin_summary():
-#     """
-#     Returns a list of rows containing:
-#         dataset_origin, number_of_datasets, first_dataset, last_dataset
-#     """
-#     with Session(engine) as session:
-#         statement = (
-#             select(
-#                 DatasetOrigin.name.label("dataset_origin"),
-#                 func.count(Dataset.dataset_id).label("number_of_datasets"),
-#                 func.min(Dataset.date_created).label("first_dataset"),
-#                 func.max(Dataset.date_created).label("last_dataset"),
-#             )
-#             .join(Dataset, Dataset.origin_id == DatasetOrigin.origin_id)
-#             .group_by(DatasetOrigin.name)
-#         )
-
-#         results = session.exec(statement).all()
-
-#         total_count = 0
-#         for row in results:
-#             total_count += row.number_of_datasets
-
-#         return results, total_count
-
 
 def get_dataset_origin_summary():
     """
@@ -46,22 +20,25 @@ def get_dataset_origin_summary():
     with Session(engine) as session:
         statement = (
             select(
+                # Dataset stats
                 DatasetOrigin.name.label("dataset_origin"),
-                # Distinct dataset count
                 func.count(func.distinct(Dataset.dataset_id)).label("number_of_datasets"),
                 func.min(Dataset.date_created).label("first_dataset"),
                 func.max(Dataset.date_created).label("last_dataset"),
 
-                # Count top-level files (is_from_zip_file == False)
-                func.count(File.file_id)
-                   .filter(File.is_from_zip_file == False)
+                # Count files that are not zip files, also is_from_zip_file == False
+                func.count(func.distinct(File.file_id))
+                   .filter(
+                       (File.is_from_zip_file == False),
+                       (FileType.name != "zip")
+                       )
                    .label("files"),
 
                 # Sum size of all files in GB
                 (func.sum(File.size_in_bytes) / 1e9).label("total_size_in_GB"),
 
                 # Count parent zip files (FileType.name == 'zip')
-                func.count(File.file_id)
+                func.count(func.distinct(File.file_id))
                    .filter(
                        (File.is_from_zip_file == False) &
                        (FileType.name == "zip")
@@ -69,63 +46,54 @@ def get_dataset_origin_summary():
                    .label("zip_files"),
 
                 # Count files that are inside zip files
-                func.count(File.file_id)
+                func.count(func.distinct(File.file_id))
                    .filter(File.is_from_zip_file == True)
                    .label("files_within_zip_files"),
 
                 # Total files (outside-non-zip_parent + parent zips + inside zips)
-                func.count(File.file_id).label("total_files"),
+                func.count(func.distinct(File.file_id)).label("total_files"),
             )
             .join(Dataset, Dataset.origin_id == DatasetOrigin.origin_id)
-            .join(File, File.dataset_id == Dataset.dataset_id)
-            .join(FileType, File.file_type_id == FileType.file_type_id, isouter=True)
+            .outerjoin(File, File.dataset_id == Dataset.dataset_id)
+            .outerjoin(FileType, File.file_type_id == FileType.file_type_id)
             .group_by(DatasetOrigin.name)
         )
 
         datasets_stats_results = session.exec(statement).all()
 
-        datasets_stats_total_count = {}
-        datasets_stats_total_count["Number of datasets"] = sum(
-            row.number_of_datasets for row in datasets_stats_results
-        )
-        datasets_stats_total_count["First dataset"] = None
-        datasets_stats_total_count["Last dataset"] = None
-        datasets_stats_total_count["Files"] = sum(
-            row.files for row in datasets_stats_results
-        )
-        datasets_stats_total_count["Total size in GB"] = sum(
-            row.total_size_in_GB for row in datasets_stats_results
-        )
-        datasets_stats_total_count["Zip files"] = sum(
-            row.zip_files for row in datasets_stats_results
-        )
-        datasets_stats_total_count["Files within zip files"] = sum(
-            row.files_within_zip_files for row in datasets_stats_results
-        )
-        datasets_stats_total_count["Total files"] = sum(
-            row.total_files for row in datasets_stats_results
-        )
-
+        datasets_stats_total_count = {
+            "Number of datasets": "{:,}".format(sum(row.number_of_datasets for row in datasets_stats_results)),
+            "First dataset": "nan",
+            "Last dataset": "nan",
+            "Files": "{:,}".format(sum(row.files for row in datasets_stats_results)),
+            "Total size in GB": "{:,.0f}".format(sum(row.total_size_in_GB for row in datasets_stats_results)),
+            "Zip files": "{:,}".format(sum(row.zip_files for row in datasets_stats_results)),
+            "Files within zip files": "{:,}".format(sum(row.files_within_zip_files for row in datasets_stats_results)),
+            "Total files": "{:,}".format(sum(row.total_files for row in datasets_stats_results)),
+        }
+        
         return datasets_stats_results, datasets_stats_total_count
-    
+
 
 # For file type stats, we want to retrieve:
 # - the number of files per file type
 # - the number of datasets per file type
 # - the total size of files per file type
-def file_type_stats():
+def get_file_type_stats():
     with Session(engine) as session:
+
         statement = (
             select(
-                FileType.name.label("file_type"),
-                func.count(File.file_id).label("number_of_files"),
-                func.count(Dataset.dataset_id).label("number_of_datasets"),
-                (func.sum(File.size_in_bytes) / 1e9).label("total_size_in_GB"),
+            FileType.name.label("file_type"),
+            func.count(func.distinct(File.file_id)).label("number_of_files"),
+            func.count(func.distinct(Dataset.dataset_id)).label("number_of_datasets"),
+            (func.sum(File.size_in_bytes) / 1e9).label("total_size_in_GB"),
             )
             .join(File, File.file_type_id == FileType.file_type_id)
-            .join(Dataset, Dataset.dataset_id == File.dataset_id)
+            .outerjoin(Dataset, Dataset.dataset_id == File.dataset_id)
             .group_by(FileType.name)
         )
     
         file_type_stats_summary = session.exec(statement).all()
+
         return file_type_stats_summary
