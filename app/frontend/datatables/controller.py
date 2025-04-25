21c08bad78294771d1d5c1a24cdc7df55a19b61b
Pierre Poulain <pierre.poulain@cupnet.net>
2025-04-25 21:39:37 +0200

    Remove deprecated datasets datatables

--- app/frontend/datatables/controller.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Request
-from fastapi.responses import JSONResponse
-
-from . import service
-
-router = APIRouter(
-    prefix="/datatables",
-    tags=["datatables"],
-)
